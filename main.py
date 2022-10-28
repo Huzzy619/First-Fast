96dd589af0f12b992f0960e8fe74acf03e7fb59a
Huzzy619 <blazingkrane@gmail.com>
2022-10-28 07:18:49 +0100

    complete

--- main.py
+++ main.py
@@ -6,5 +6,11 @@ app = FastAPI()
 
 
 @app.get("/")
-async def new():
-    return {"hello":"hi"}
+async def index():
+    bio = "I am a Python programmer, I use Django, FastAPI and Django RestFramework for Backend Web development "
+    return {
+        "slackUsername": "Huzzy-K",
+        "backend": True,
+        "age": 24,
+        "bio": bio 
+        }
